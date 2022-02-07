e81b483396dc52e9835f78c8a5fb3a90516dc7aa
Ciri Ryan <cirijaye@yahoo.com>
2022-02-07 04:32:43 -0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const message = require('./myscript');
+// const message = require('./myscript');
 
-console.log(message);
+// console.log(message);
 
+const counterObject = require('./myscript.js');
+
+console.log(counterObject.getCounter());//0
+counterObject.incrementCounter();
+console.log(counterObject.getCounter()); //1
+
+// will be the same counter instance
+const newCounter = require('./myscript.js');
+console.log(newCounter.getCounter()); //1
